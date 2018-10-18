6fd48dfada74cb2ad9b65491387df5b1738c2c94
josepm <josepm>
2018-10-18 20:43:53 +0200

    New article model and modified user model

---
+++ node-server/models/articles.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let ArticleSchema = new mongoose.Schema(
+    {
+        text: String,
+        title: String,
+        description: String,
+        thumbnail: String,
+        likes: Number,
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        comments: [
+            {
+                author: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'User'
+                },
+                text: String
+            }
+        ]
+    }
+);
+
+module.exports = mongoose.model('Article', ArticleSchema)
--- node-server/models/user.js
+++ node-server/models/user.js
@@ -8,7 +8,7 @@ const crypto = require('crypto')
 const UserSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     displayName: String,
-    avatar: String,
+    username: String,
     password: { type: String, select: false },
     signupDate: { type: Date, default: Date.now() },
     lastLogin: Date
@@ -29,12 +29,5 @@ UserSchema.pre('save', (next) => {
         })
     })
 })
-
-UserSchema.methods.gravatar = function () {
-    if (!this.email) return `https://gravatar.com/avatar/?s=200&d=retro`
-
-    const md5 = crypto.createHash('md5').update(this.email).digest('hex')
-    return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
-}
 
 module.exports = mongoose.model('User', UserSchema)
