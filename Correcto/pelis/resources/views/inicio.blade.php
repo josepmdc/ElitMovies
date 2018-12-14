9fd44d40c829e9d29e0b0302e64c5269bb02ee6b
josepm98 <jdomingocatafal@gmail.com>
2018-12-14 11:44:07 +0100

    Borrado carpetas que no usamos

--- Correcto/pelis/resources/views/inicio.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('base')
-@section('tituloPagina') Inicio @endsection
-@section('EstiloPlus') 
-<link rel="stylesheet" type="text/css" href="{{url('css/'.$datos['Estilo'].'.css')}}">
-@endsection
-
