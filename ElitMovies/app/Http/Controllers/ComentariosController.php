a82b57ef6683b9e80cda51552857f729375d4f92
Josep M Domingo <jdomingocatafal@gmail.com>
2019-01-05 17:39:25 +0100

    Subcomentarios dentro de el objeto de Comentarios
    He modificad los comentarios para que con cada comentario nos vengan todos las respuestas a este comentario, para así poder cargarlas y mostrar cuantas respuestas tiene cada comentario.

--- ElitMovies/app/Http/Controllers/ComentariosController.php
+++ ElitMovies/app/Http/Controllers/ComentariosController.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;//Necesario para solicitudes
 use DB; 
 use App;
+
+
 class ComentariosController extends Controller
 {
     /**
@@ -28,69 +32,42 @@ class ComentariosController extends Controller
         
     }
 
-    public function ListadoComentarios(Request $request) 
     //Listado de comentarios por Id de pelicula sin ser SubComentario de ninguno
+    public function ListadoComentarios(Request $request) 
     {
-
-
         $comentarios =  
         DB::table('Comentario')
             ->where('Pelicula_Id',          '=',$request->input('IdPelicula'))
             ->where('id_SubComentarioDe',   '=',0)
             ->orWhere('id_SubComentarioDe', '=',null)
             ->Paginate(15);
-        return json_encode($comentarios);
 
+        // Convertimos el JSON a array para poder añadir los subcomentarios
+        $comentariosArray = json_decode($comentarios->toJson(), true);
+        $data = $comentariosArray['data'];
+
+        // Por cada comentario busca los subcomentarios y los añade al array
+        foreach ($data as &$comentario) {
+            $comentario['subComentarios'] =  $this->ListadoSubComentarios($comentario);
+        }
+
+        $comentariosArray['data'] = $data;
+        
+        // Devuelve el array codificado a JSON otra vez
+        return json_encode($comentariosArray);
     }
 
 
-    public function ListadoSubComentarios(Request $request) 
     //Listado de comentarios por Id de Pelicula y de Comentario padre
+    public function ListadoSubComentarios($comentario) 
     {
         $comentarios =  
         DB::table('Comentario')
-            ->where('Pelicula_Id',          '=',$request->input('IdPelicula'))
-            ->where('id_SubComentarioDe',   '=',$request->input('IdComentario'))
-            ->Paginate(15);
-        return $comentarios;
+            ->where('Pelicula_Id',          '=',$comentario['Pelicula_Id'])
+            ->where('id_SubComentarioDe',   '=',$comentario['id'])
+            ->get();
+        
+        return json_decode($comentarios);
     }
-
-
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
